b57b0fc7c2d9a9211cf8980f2ebb1ade9efd27a0
Ryloo77 <rilou_formeinweb@hotmail.com>
2023-01-10 17:48:26 +0100

    carrousel 25%

--- src/pages/Erreur.js
+++ src/pages/Erreur.js
@@ -1,9 +1,14 @@
+import '../styles/Erreur.css'
+import  {Link} from 'react-router-dom'
+
 function ErrorPage () {
 
     return (
-        <>
-        Erreur 404
-        </>
+        <div className='erreur'>
+            <h2 className='erreur-title'>404</h2>
+            <p className='erreur-msg'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link className='erreur-link'to="/">Retourner sur la page d'accueil</Link>
+        </div>
     )
 }
 
