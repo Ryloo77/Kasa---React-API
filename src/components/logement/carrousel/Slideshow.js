4ea118c0a3d88fcc5265b69f30eea1bc4476342d
Ryloo77 <rilou_formeinweb@hotmail.com>
2023-01-31 09:02:21 +0100

    correct error logement

--- src/components/logement/carrousel/Slideshow.js
+++ src/components/logement/carrousel/Slideshow.js
@@ -19,15 +19,13 @@ function ImageSlider() {
     }
     
     const nextSlide = () => {
-        // si on arrive sur la dernière image (pour 4 images, 4-1 est égale a la 4eme image du tableau [3])
-        // on retourne au début (image [0]), sinon on incrément de 1
         setCurrent(current === length - 1 ? 0 : current + 1)
     }
 
     return (
         <div className='conteneur'>
-            <button key={slides[1] + { length }} className={length === 1 ? "carrousel-btn--hidden" : 'carrousel-btn--G'}><img src={vector} alt="" onClick={prevSlide} /></button>
-            <button key={slides[2] + { length }} className={length === 1 ? "carrousel-btn--hidden" : 'carrousel-btn--D'}><img src={vector} alt="" onClick={nextSlide} /></button>
+            <button  className={length === 1 ? "carrousel-btn--hidden" : 'carrousel-btn--G'}><img src={vector} alt="" onClick={prevSlide} /></button>
+            <button  className={length === 1 ? "carrousel-btn--hidden" : 'carrousel-btn--D'}><img src={vector} alt="" onClick={nextSlide} /></button>
 
             {product.pictures.map((picture, index) => {
                 return (
