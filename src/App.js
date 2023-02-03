09a0380881a22ae0b89af7671f58b3634061a81f
Ryloo77 <rilou_formeinweb@hotmail.com>
2023-02-03 20:07:59 +0100

    App in src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/home/Home';
-import Logement from '../pages/logement/Logement';
-import ErrorPage from '../pages/Erreur';
-import Apropos from '../pages/Apropos'
-import '../styles/App.css'
-import Layout from '../pages/Layout';
+import Home from './pages/home/Home';
+import Logement from './pages/logement/Logement';
+import ErrorPage from './pages/Erreur';
+import Apropos from './pages/Apropos'
+import './styles/App.css'
+import Layout from './pages/Layout';
 
 function App() {
 
